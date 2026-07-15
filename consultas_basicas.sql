762ea260398d9e428a963c8060988f75c59ca309
milumorales2 <milumorales2@gmail.com>
2026-07-14 21:56:49 -0300

    Update consultas_basicas.sql

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -5,7 +5,7 @@
 -- ══════════════════════════════════════════
 -- Consulta 1: Exploración general de la tabla sales
 
-SELECT * FROM sales; -- Se utiliael * para visualizar la tabla entera, útil cuando se requieren ver todas las columnas juntas si o si, no se recomienda si hay columnas que no se necesitar, especificamente cuando la base de datos es muy grande ya que demora más tiempo en procesarse la consulta
+SELECT * FROM sales; -- Se utilizael * para visualizar la tabla entera, útil cuando se requieren ver todas las columnas juntas si o si, no se recomienda si hay columnas que no se necesitan.
 
 -- Consulta 2: Selección de columnas específicas para finanzas
 
@@ -13,4 +13,4 @@ SELECT customer_id, product_id, total_amount FROM sales;
 
 -- Consulta 3: Selección con alias en español para stakeholders
 
-SELECT order_date AS fecha_pedido, product_name AS nombre_pedido, quantity AS cantidad_unidades;
+SELECT order_date AS fecha_pedido, product_name AS nombre_producto, quantity AS cantidad_unidades FROM ;
